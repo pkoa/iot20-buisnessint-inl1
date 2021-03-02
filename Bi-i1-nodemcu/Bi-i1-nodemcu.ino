697283edaf86a6123272b712a07baa95ddd3e7e0
pkoa <71134596+pkoa@users.noreply.github.com>
2021-03-02 14:20:58 +0100

    inital commit

--- Bi-i1-nodemcu/Bi-i1-nodemcu.ino
+++ Bi-i1-nodemcu/Bi-i1-nodemcu.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "functions.h"
+#include "passwords.h"
 
+#define DEVICE_ID "nodemcu"
+#define MSG_LEN 256
+#define SENSOR_MIN 0
+#define SENSOR_MAX 1024
+#define READ_PIN 15
+
+//ssid, pass och connection_string ligger i gitignorad passwords.h fil
+
+
+int prev_sensor_val = 0;
+String formatted_time;
+
+void setup() {
+  Serial.begin(115200);
+  delay(3000);
+  initWifi(ssid, pass);
+  initMqtt(connection_string);
+  timeClient.begin();
+  timeClient.setTimeOffset(3600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  int sensor_value = digitalRead(2);
+  Serial.println(sensor_value);
+  Serial.println(digitalRead(3));
+  Serial.println(digitalRead(4));
+  Serial.println(digitalRead(5));
+  delay(300);
+  if(!pending){
+    
+  }
+  
 }
