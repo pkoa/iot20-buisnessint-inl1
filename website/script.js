2220c492d1b2d591cc3710fb0a226554b048c6cf
pkoa <71134596+pkoa@users.noreply.github.com>
2021-03-05 14:52:14 +0100

    updated website

--- website/script.js
+++ website/script.js
@@ -1,10 +1,17 @@
 /* script file */
-let tabledata = document.getElementById('tabledata')
+let tabledata1 = document.getElementById('tabledata-feather')
+let tabledata2 = document.getElementById('tabledata-nodemcu')
+let tabledata3 = document.getElementById('tabledata-unidentified')
 
 fetch("https://cosmosdb-iot20-save-get.azurewebsites.net/api/GetFromCosmosdb?code=oMXAifKJkaXdKftp7rBnvlwQTmzWWrsupNTHstcgI9qJlMJi30Y04w==")
 .then(res => res.json())
 .then(data =>{
     for(let row of data){
-        tabledata.innerHTML += `<tr class="table-dark"><td>${row.id}</td><td>${row.deviceid}</td><td>${row.temperature}</td><td>${row.humidity}</td><td>${row.halleffect}</td><td>${row.formatted_time}</td>`
+        if(row.deviceid == "feather")
+            tabledata1.innerHTML += `<tr class="table-dark"><td>${row.id}</td><td>${row.deviceid}</td><td>${row.temperature}°C</td><td>${row.humidity}%</td><td>${row.formatted_time}</td>`
+        else if(row.deviceid == "nodemcu")
+            tabledata2.innerHTML += `<tr class="table-dark"><td>${row.id}</td><td>${row.deviceid}</td><td>${row.halleffect}</td><td>${row.formatted_time}</td>`
+        else
+            tabledata3.innerHTML += `Unidentified device ${row.deviceid} with # ${row.id}`
     }
 })
